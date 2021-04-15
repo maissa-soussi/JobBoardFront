68804179abb250e18c1c4163dfba47a5fc7946bc
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-04-15 18:13:09 +0100

    ADMIN dashboard updates

--- src/app/footer-admin/footer-admin.component.ts
+++ src/app/footer-admin/footer-admin.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer-admin',
   templateUrl: './footer-admin.component.html',
-  styleUrls: ['./footer-admin.component.css']
+  styleUrls: ['../dashboard/dashboard.component.css']
 })
 export class FooterAdminComponent implements OnInit {
   date = new Date().getFullYear();

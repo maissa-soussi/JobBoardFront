d845b5e8dbe7f5737b5f303633d55b20c663abc2
farouk kassebi <farouk.kassebi@ensi-uma.tn>
2021-04-27 15:56:31 +0100

    update joboffer component

--- src/app/admin-offres/admin-offres.component.ts
+++ src/app/admin-offres/admin-offres.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminOffresService } from './admin-offres.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-offres',
@@ -8,10 +9,45 @@ import { AdminOffresService } from './admin-offres.service';
 })
 export class AdminOffresComponent implements OnInit {
   offres: any=[];
-  constructor(private myservice: AdminOffresService) { }
+  public Countries: any[] = []
+  public Domaines: any[] = []
+  public Experiences: any[] = []
+  public Contrats: any[] = []
+  public Diplomes: any[] = []
+  constructor(private myservice: AdminOffresService, public http: HttpClient) { }
 
   ngOnInit(): void {
     this.getalloffres();
+      //get Countries
+      this.http.get<any>("https://localhost:44338/Countries")
+      .subscribe(
+        (result) => { this.Countries = result },
+        (error) => { console.log(error) }
+      )
+      //get Diplomes
+      this.http.get<any>("https://localhost:44338/Diplomas")
+      .subscribe(
+        (result) => { this.Diplomes = result },
+        (error) => { console.log(error) }
+      )
+      //get Domaines
+      this.http.get<any>("https://localhost:44338/Domains")
+      .subscribe(
+        (result) => { this.Domaines = result },
+        (error) => { console.log(error) }
+      )
+      //get Experiences
+      this.http.get<any>("https://localhost:44338/Experiences")
+      .subscribe(
+        (result) => { this.Experiences = result },
+        (error) => { console.log(error) }
+      )
+      //get contrattype
+      this.http.get<any>("https://localhost:44338/ContratTypes")
+      .subscribe(
+        (result) => { this.Contrats = result },
+        (error) => { console.log(error) }
+      )
   }
   getalloffres()
   {

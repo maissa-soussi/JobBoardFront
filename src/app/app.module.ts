e933c6c80c61006d294bf965b842a06f8158884f
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-04-27 01:47:02 +0100

    loginFormControls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -50,7 +50,9 @@ import { MesCandidaturesComponent } from './mes-candidatures/mes-candidatures.co
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormControl, FormBuilder, Validators} from "@angular/forms"
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  public loginForm: FormGroup
+  public erreur:String=""
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { 
+    let loginFormControls = {
+      email: new FormControl("",[
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl("",[
+        Validators.required,
+        Validators.minLength(8)
+      ])
+    }
+    this.loginForm= formBuilder.group(loginFormControls)
+  }
+  get email() {return this.loginForm.get('email')}
+  get password() {return this.loginForm.get('password')}
   ngOnInit(): void {
   }
 

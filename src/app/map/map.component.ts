301637f98eb33de654bda056555c9cdd8b690043
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-04-28 17:33:20 +0100

    marker maps

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,21 +7,22 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements OnInit {
   private map: L.Map;
-  private centroid: L.LatLngExpression = [42.3601, -71.0589];
+  private centroid: L.LatLngExpression = [36.800, 10.1864];
   constructor() { }
 
   private initMap(): void {
     this.map = L.map('map',{
       center: this.centroid,
-      zoom: 12
+      zoom: 1
     });
     const tiles = L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=57XMBquwc7haRCgQDYWX', {
-      maxZoom: 18,
+      maxZoom: 24,
       minZoom: 10,
       attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
     });
-
+    
     tiles.addTo(this.map);
+    var marker = L.marker([36.85045,10.19235 ]).addTo(this.map);
   }
 
   ngOnInit(): void {

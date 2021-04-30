2e270476c00beaabef28177f0f4f18c91645d7c0
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-04-30 16:42:24 +0100

    admin crud

--- src/app/gestion-admins/gestion-admins.component.ts
+++ src/app/gestion-admins/gestion-admins.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../parametre/parametre.component.css']
 })
 export class GestionAdminsComponent implements OnInit {
-
-  constructor() { }
-
+  public admins: any[] = []
+  constructor(public http: HttpClient) { }
+  myadmin:any={};
   ngOnInit(): void {
+     //get admins
+     this.http.get<any>("https://localhost:44338/Users")
+     .subscribe(
+       (result) => { this.admins = result },
+       (error) => { console.log(error) }
+     )
+  }
+
+  //delete admin
+  adminDelete(id:number){
+    if (confirm('Voulez vous supprimé cet administrateur ?'))
+      {
+        this.http.delete(`https://localhost:44338/Users/${id}`)
+      .subscribe(
+        err =>{console.log(err)}
+      );
+      window.location.reload()
+      }
+  }
+
+  //add admin
+  addAdmin(){
+    this.myadmin.role ="admin";
+    this.http.post("https://localhost:44338/Users",this.myadmin).subscribe(
+      (data)=>{
+        alert("Ajouté avec succès");
+        return data;
+      },
+      (err)=>{
+        alert("Admin existe deja");
+        console.log(err);
+      }
+    );
+    window.location.reload()
   }
 
 }

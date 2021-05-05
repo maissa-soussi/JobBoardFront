4e0a94e2d524eed681ace802099c739b063b35b0
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-05-05 19:06:16 +0100

    profile condidat css

--- src/app/sidebar-admin/sidebar-admin.component.ts
+++ src/app/sidebar-admin/sidebar-admin.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar-admin',
   templateUrl: './sidebar-admin.component.html',
-  styleUrls: ['../dashboard/dashboard.component.css']
+  styleUrls: ['../dashboard/dashboard.component.css','./sidebar-admin.component.css']
 })
 export class SidebarAdminComponent implements OnInit {
   collapseShow = "hidden";

bd7031937dce66468cfcbe0f94700ada4239b77e
ahmedbenalaya30 <ahmed.benalaya@ensi-uma.tn>
2021-05-08 16:23:31 +0000

    mes candidatures(+ spontaté)

--- src/app/mes-candidatures/mes-candidatures.component.ts
+++ src/app/mes-candidatures/mes-candidatures.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 })
 export class MesCandidaturesComponent implements OnInit {
   public mesCandidatures: any[] = []
+  public mesCandidatureSponts: any[] = []
   public id:any={}
 
 
@@ -19,11 +20,16 @@ export class MesCandidaturesComponent implements OnInit {
     let role = localStorage.getItem("role")
     if (role =="admin")
     this.router.navigateByUrl('/'); 
-    
+
     this.id = localStorage.getItem("id")
     this.http.get<any>("https://localhost:44338/MyCandidatures/"+this.id)
     .subscribe(
       (result) => { this.mesCandidatures = result },
+      (error) => { console.log(error) }
+    )
+    this.http.get<any>("https://localhost:44338/MyCandidatureSponts/"+this.id)
+    .subscribe(
+      (result) => { this.mesCandidatureSponts = result },
       (error) => { console.log(error) }
     )
   }

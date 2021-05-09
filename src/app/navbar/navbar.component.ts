4e149e60e405bb9f1e756536f311bfd8675cf53b
Maissa Soussi <maissa.soussi1920@yahoo.com>
2021-05-09 17:17:04 +0100

    navbar test

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  public test : boolean = false
+  public role : boolean 
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+    if (localStorage.length!=0)
+    {
+      this.test=true
+      if (localStorage.getItem("role")=="admin")
+      {
+        this.role = true
+      }
+      else {
+        this.role = false
+      }
+    }
+
+    
+  }
+
+
+  
+  logout()
+  {
+    localStorage.removeItem("mytoken");
+    localStorage.removeItem("role");
+    localStorage.removeItem("id");
+    localStorage.removeItem("nom");
+    localStorage.removeItem("prenom");
+    this.router.navigateByUrl('/');
   }
 
 }
